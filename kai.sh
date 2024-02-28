44d194052b5fbc365da443f82611296cc32b6f1c
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-02-28 22:13:02 +0800

    Update kai.sh

--- kai.sh
+++ kai.sh
@@ -1,5 +1,5 @@
 for ((i=1; i<=765; i++))  
 do  
-nohup curl -o /tmp/sbrz http://idcef.com/themes/clientarea/IDC01/assets/images/sec1.jpg -x 156.224.26.8:443  > /tmp/idc01x${i}-start.log 2>&1 &
+nohup curl -o /tmp/sbrz http://www.idcef.com/themes/clientarea/IDCEF/assets/images/sec1.jpg -x 156.224.26.8:443  > /tmp/idc01x${i}-start.log 2>&1 &
 done
 echo ok
